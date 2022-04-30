5ac19bf1f5ca5b8b1820a78183af8cc4e8fb2a80
yogitasingla17 <101088130+yogitasingla17@users.noreply.github.com>
2022-04-30 15:41:20 +0530

    CSS updtaed

--- assets/js/main.js
+++ assets/js/main.js
@@ -189,6 +189,69 @@
     }
   });
 
+   /**
+   * Testimonials slider
+   */
+    new Swiper('.testimonials-slider', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+  
+        1200: {
+          slidesPerView: 3,
+        }
+      }
+    });
+      /**
+   * Clients Slider
+   */
+  new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120
+      }
+    }
+  });
+
   /**
    * Animation on scroll
    */
